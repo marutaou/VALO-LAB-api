236b7037e8329018d5d4c562891a2e9b868607d2
marutaou <sota19980515@gmail.com>
2025-02-05 00:02:15 +0900

    PORT変更

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 const postsRouter = require("./router/posts");
 const authRouter = require("./router/auth");
